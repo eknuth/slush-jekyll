bad14e0762ac396ee050242d3b5c946d4f08a73c
endi <endisukaj@gmail.com>
2015-11-16 01:40:12 +0100

    Added support for creating drafts.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+'use strict';
 var gulp = require('gulp'),
     install = require('gulp-install'),
     conflict = require('gulp-conflict'),
@@ -9,9 +10,10 @@ var gulp = require('gulp'),
 
 gulp.task('post', function (done) {
   var now = moment();
-
+  var dirname, basename, prefix, extname = ".md";
   inquirer.prompt([
-    {type: 'input', name: 'title', message: 'Title of post', default: gulp.args.join(' ')},
+    {type: 'list', name: 'type', message: 'Type of post', choices: ['Post', 'Draft']},
+    {type: 'input', name: 'title', message: 'Title of post'},
     {type: 'input', name: 'description', message: 'Description of post'},
     {type: 'confirm', name: 'moveon', message: 'Continue?'}
   ],
@@ -19,13 +21,25 @@ gulp.task('post', function (done) {
     if (!answers.moveon) {
       return done();
     }
+
+    // Check type of post, if it's to be published publish it to _posts, otherwise, save it as draft
+    if (answers.type === 'Post'){
+      dirname = '_posts/' + now.year();
+      basename = S(answers.title).slugify();
+      prefix = now.format('YYYY-MM-DD-');
+    } else {
+      dirname = '_drafts/' + now.year();
+      basename = S(answers.title).slugify();
+      prefix = '';
+      extname = '.md';
+    }
     gulp.src(__dirname + '/templates/post.html')
       .pipe(template(answers))
       .pipe(rename({
-        dirname: "_posts/" + now.year(),
-        basename: S(answers.title).slugify(),
-        prefix: now.format('YYYY-MM-DD-'),
-        extname: ".md"
+        dirname: dirname,
+        basename: basename,
+        prefix: prefix,
+        extname: extname
       }))
       .pipe(conflict('./'))
       .pipe(gulp.dest('./'))                   // Without __dirname here = relative to cwd
